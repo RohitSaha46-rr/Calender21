461159df1d6f4bbbf930c4c1d04eba4b08ab3e0c
RohitSaha46-rr <76219397+RohitSaha46-rr@users.noreply.github.com>
2021-06-10 10:05:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ let table=document.getElementById('table');
 let country=document.getElementById('country');
 let main_content=document.getElementById('main_content');
 let footer=document.querySelector('.footer');
-const fun=()=>{
+let form1=document.querySelector('.form1');
+form1.addEventListener('submit',fun=(e)=>{
+e.preventDefault();
    
  
 async function calender(){
@@ -42,4 +44,41 @@ window.open("error.html");
 }
 }
 calender();
+});
+
+/*---------------------------------------------------------------for button submission-------------------------------------*/
+function fun1(){
+    async function calender(){
+        try{
+        const data=await fetch('https://calendarific.com/api/v2/holidays?&api_key='+api_key+'&country='+country.value+'&year=2021');
+        const jsondata = await data.json();
+        console.log(jsondata);
+        let holiday=jsondata.response.holidays[0].name;
+        
+        console.log(holiday);
+        let array=jsondata.response.holidays;
+        let html="";
+        array.forEach(element => {
+            let name=element.name;
+            let description=element.description;
+            let iso=element.date.iso;
+            
+            html+=`
+            <table> 
+            <td id="dte">${iso}</td>
+            <td id="name_holiday">${name}</td>
+            <td id="desc_holiday">${description}</td>
+            </table>`;
+           
+        });
+        main_content.innerHTML=html;
+        footer.innerHTML=`<footer>
+        <p>Copyright ©2021 All rights reserved | This template is made with love by Rohit Saha</p>
+    </footer>`;
+        }
+        catch (error) {
+    window.open("error.html");
+    }
+    }
+    calender();
 }
